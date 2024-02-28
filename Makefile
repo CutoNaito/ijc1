1b7af2646ef64f910c717ebf9fbbecb92660beab
CutoNaito <cutonaito@github.com>
2024-02-28 21:33:03 +0100

    finite-state automata wip

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ CFLAGS = -g -std=c11 -pedantic -Wall -Wextra -lm
 TARGET = bin/primes bin/primes-i
 
 prime_src = src/main.c src/error.c src/eratosthenes.c
+no-comment_src = src/no-comment.c src/error.c
 
 all: primes primes-i no-comment
 
@@ -24,4 +25,4 @@ run:
 	ulimit -s 85000 && ./bin/primes-i
 
 no-comment:
-	$(CC) $(CFLAGS) -o bin/no-comment src/no-comment.c
+	$(CC) $(CFLAGS) -o bin/no-comment $(no-comment_src)
--- src/no-comment.c
+++ src/no-comment.c
@@ -3,12 +3,18 @@
 
 int main(int argc, char **argv) 
 {
-    if (argc == 1 || argc > 2)
+    if (argc > 2)
         error_exit("Neplatny pocet argumentu.");
 
-    FILE *fp = fopen(argv[1], "r");
-    if (fp == NULL)
-        error_exit("Soubor se nepodarilo otevrit.");
+    FILE *fp;
+    if (argc == 1)
+        fp = stdin;
+
+    if (argc == 2) {
+        fp = fopen(argv[1], "r");
+        if (fp == NULL)
+            error_exit("Soubor se nepodarilo otevrit.");
+    }
 
     /* Finite-state machine */
     int state = 0;
@@ -19,9 +25,102 @@ int main(int argc, char **argv)
         case 0:
             if (c == '/') {
                 state = 1;
+            } else if (c == '"') {
+                putchar(c);
+                state = 6;
+            } else if (c == '\'') {
+                putchar(c);
+                state = 8;
             } else {
                 putchar(c);
             }
+
+            break;
+
+        case 1:
+            if (c == '/') {
+                state = 4;
+            } else if (c == '*') {
+                state = 2;
+            } else {
+                putchar('/');
+                putchar(c);
+                state = 0;
+            }
+
+            break;
+
+        case 2:
+            if (c == '*') {
+                state = 3;
+            }
+
+            break;
+
+        case 3:
+            if (c == '/') {
+                putchar(' ');
+                state = 0;
+            } else if (c != '*') {
+                state = 2;
+            }
+
+            break;
+
+        case 4:
+            if (c == '/') {
+                state = 5;
+            }
+
+            break;
+
+        case 5:
+            if (c != '/') {
+                state = 4;
+            }
+
+            break;
+
+        case 6:
+            if (c == '\\') {
+                putchar(c);
+                state = 7;
+            } else if (c == '"') {
+                putchar(c);
+                state = 0;
+            } else {
+                putchar(c);
+            }
+
+            break;
+
+        case 7:
+            putchar(c);
+            state = 6;
+
+            break;
+        
+        case 8:
+            if (c == '\\') {
+                putchar(c);
+                state = 9;
+            } else if (c == '\'') {
+                putchar(c);
+                state = 0;
+            } else {
+                putchar(c);
+            }
+
+            break;
+
+        case 9:
+            putchar(c);
+            state = 8;
+
+            break;
+
+        default:
+            error_exit("Nastala chyba ve stavu.");
         }
     }
 
